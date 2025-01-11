489639f517df64d63d6f4e4685c6c203da051aed
rahulap-aot <rahul.ap@aot-technologies.com>
2025-01-10 23:58:38 -0800

    login and sign up design

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { MeterialModule } from '../../../Meterial.Module';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MeterialModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  isSignUp: boolean = true;
+
+  readonly username = new FormControl('',{
+    validators : [
+      Validators.required,
+      Validators.minLength(3),
+    ]
+  });
+
+
+  onLogin(){
+    this.username.markAsTouched();
+
+    if (this.username.valid) {
+      console.log('No errors. Form is valid!');
+      console.log('Username:', this.username.value);
+    }
+  }
+
+  onTabSwitch(){
+    this.isSignUp = !this.isSignUp;
+    this.username.reset();
+  }
 }
