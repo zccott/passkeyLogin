4e913c60c098520c20b8d5d04236ab21fa53be12
rahulap-aot <rahul.ap@aot-technologies.com>
2025-01-10 21:57:28 -0800

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'home',
+                component: HomeComponent
+            }
+        ]
+    },
+];
---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
